eb5b57a6b9ebf4d2b72a2d8a8d011bcc8bf709d2
SviatoslavPaseka <paseka.syat@gmail.com>
2023-10-31 13:23:49 +0200

    first commit

--- jmeter/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#bash ./bin/jmeter.sh -n -t ${SCENARIO} -j ${WORKSPACE}/jmeter.log -l ${WORKSPACE}/report.log -e -o ${WORKSPACE}/report/ -JUSERS=${USERS} -JRAMPUP=${RAMPUP} -JDURATION=${DURATION} -JBUILD_NUMBER=${BUILD_NUMBER} -JBUILD_URL=${BUILD_URL} -JSCENARIO_NAME=${SCENARIO_NAME}
-bash ./bin/jmeter.sh -n -t /jmeter/scenarios/multiScenario.jmx -l /jmeter/test-results.jtl
